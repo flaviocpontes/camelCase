8985642cea28d708b9b4905a0f2369f78016e8ae
Flávio Pontes <flaviopontes@acerp.org.br>
2016-05-30 14:42:45 -0300

    Teste da string "Nome" passando.

--- src/CamelCase.java
+++ src/CamelCase.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class CamelCase {
     public static List<String> converterCamelCase(String original) {
         List<String> palavras = new ArrayList<>();
-        if (original.length() > 0) palavras.add(original);
+        if (original.length() > 0) palavras.add(original.toLowerCase());
         return palavras;
     }
 }
