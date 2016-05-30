2081c90c42a5801ebc20649040089e3f56c7c8ee
Flávio Pontes <flaviopontes@acerp.org.br>
2016-05-30 16:03:42 -0300

    Teste EncontreUmaSigla e, por conseguinte, EncontreUmaSigla passando;

--- src/CamelCase.java
+++ src/CamelCase.java
@@ -11,12 +11,17 @@ public class CamelCase {
 
     public static String acharProximaPalavra(List<String> palavras, String s) {
         for (int i = 0; i < s.length(); i++) {
-            if (Character.isUpperCase(s.charAt(i)) && i > 0){
-                palavras.add(s.substring(0, i).toLowerCase());
+            if ((Character.isUpperCase(s.charAt(i)) && i > 0) && !(Character.isUpperCase(s.charAt(Math.min(i+1, s.length()-1))))){
+                String palavra = s.substring(0, i);
+                if (Character.isLowerCase(palavra.charAt(1))) palavra = palavra.toLowerCase();
+                palavras.add(palavra);
                 return s.substring(i);
             }
         }
-        if (s.length() > 0) palavras.add(s.toLowerCase());
+        if (s.length() > 0) {
+            if (Character.isLowerCase(s.charAt(1))) s = s.toLowerCase();
+            palavras.add(s);
+        }
         return "";
     }
 }
