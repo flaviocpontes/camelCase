d620f3cc38a4856eed651f29967297b8c50f0d03
Flávio Pontes <flaviopontes@acerp.org.br>
2016-05-30 19:19:40 -0300

    Modificações em curso para fazer passar o testConvertePalavrasComNumeroIncluso.

--- src/CamelCase.java
+++ src/CamelCase.java
@@ -13,12 +13,14 @@ public class CamelCase {
 
     public static String acharProximaPalavra(List<String> palavras, String s) {
         for (int i = 0; i < s.length(); i++) {
-            if ((Character.isUpperCase(s.charAt(i)) && i > 0) && (((i < s.length() - 2) && Character.isLowerCase(s.charAt(i + 1))) || (Character.isLowerCase(s.charAt(i - 1))))) {
+            if ((Character.isUpperCase(s.charAt(i))  && i > 0) && (((i < s.length() - 2) && Character.isLowerCase(s.charAt(i + 1))) || (Character.isLowerCase(s.charAt(i - 1))))){
                 palavras.add(ajustaCapitalizacao(s.substring(0, i)));
-                return s.substring(i);}
-            if (Character.isDigit(s.charAt(i)) && i > 0) {
+                return s.substring(i);
+            } else if ((Character.isDigit(s.charAt(i)) && i > 0) && !(Character.isDigit(s.charAt(i-1)))) {
                 palavras.add(ajustaCapitalizacao(s.substring(0, i)));
-                return s.substring(i);}}
+                return s.substring(i);
+            }
+            }
         if (s.length() > 0) palavras.add(ajustaCapitalizacao(s));
         return "";
     }
