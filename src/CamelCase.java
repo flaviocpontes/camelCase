f3cc23763dc14152e504d7fa14e06294ae52461f
Flávio Pontes <flaviopontes@acerp.org.br>
2016-05-30 18:57:15 -0300

    testEncontraNumeroApensado passando!

--- src/CamelCase.java
+++ src/CamelCase.java
@@ -5,21 +5,26 @@ public class CamelCase {
     public static List<String> converterCamelCase(String original) {
         List<String> palavras = new ArrayList<>();
         String restante = original;
-        do {restante = acharProximaPalavra(palavras, restante);} while (restante.length() > 0);
+        do {
+            restante = acharProximaPalavra(palavras, restante);
+        } while (restante.length() > 0);
         return palavras;
     }
 
     public static String acharProximaPalavra(List<String> palavras, String s) {
         for (int i = 0; i < s.length(); i++) {
-            if ((Character.isUpperCase(s.charAt(i)) && i > 0) && (((i < s.length() - 2) && Character.isLowerCase(s.charAt(i+1))) || (Character.isLowerCase(s.charAt(i-1))))){
-                    palavras.add(ajustaCapitalizacao(s.substring(0, i)));
-                    return s.substring(i);}}
+            if ((Character.isUpperCase(s.charAt(i)) && i > 0) && (((i < s.length() - 2) && Character.isLowerCase(s.charAt(i + 1))) || (Character.isLowerCase(s.charAt(i - 1))))) {
+                palavras.add(ajustaCapitalizacao(s.substring(0, i)));
+                return s.substring(i);}
+            if (Character.isDigit(s.charAt(i)) && i > 0) {
+                palavras.add(ajustaCapitalizacao(s.substring(0, i)));
+                return s.substring(i);}}
         if (s.length() > 0) palavras.add(ajustaCapitalizacao(s));
         return "";
     }
 
     public static String ajustaCapitalizacao(String s) {
-        if (Character.isUpperCase(s.charAt(0))){
+        if (Character.isUpperCase(s.charAt(0))) {
             if (s.length() > 1 && (Character.isUpperCase(s.charAt(1)))) return s;
             else return s.toLowerCase();
         }
