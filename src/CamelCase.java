ff473af21ccff8eecffb3bae23399cdf06cce06f
Flávio Cardoso Ferreira Pontes <flaviocpontes@gmail.com>
2016-05-30 23:41:42 -0300

    teste testInicioPalavraSiglaIntercalada passou!

--- src/CamelCase.java
+++ src/CamelCase.java
@@ -32,8 +32,10 @@ public class CamelCase {
     }
 
     public static boolean inicioPalavra(String s, int i) {
-        if ((Character.isUpperCase(s.charAt(i)) && i > 0) && ((i < s.length() - 2) && Character.isLowerCase(s.charAt(i + 1)))) return true;
-        if ((Character.isDigit(s.charAt(i)) && i > 0) && !(Character.isDigit(s.charAt(i - 1)))) return true;
-        return false;
+        if (Character.isUpperCase(s.charAt(i)) && i > 0) {
+            if ((i < s.length() - 2) && Character.isLowerCase(s.charAt(i + 1))) return true;
+            if (Character.isLowerCase(s.charAt(i - 1))) return true;
+        }
+        return ((Character.isDigit(s.charAt(i)) && i > 0) && !(Character.isDigit(s.charAt(i - 1))));
     }
 }
